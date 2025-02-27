8f49106b515b0fb691dc3022b8bd72c4c9842a77
Rics <danil.klementev.29@gmail.com>
2025-02-27 08:55:52 +0200

    feat: add yser controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { AuthDto } from '../auth/dto/auth.dto'
+import { RefreshTokenDto } from '../auth/dto/refresh-token.dto'
+import { Auth } from '../auth/decorators/auth.decorator'
+import { CurrentUser } from '../auth/decorators/user.decorator'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') userId: string) {
+		return this.authService.login(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login/access-token')
+	async getNewTokens(@Body() dto: RefreshTokenDto) {
+		return this.authService.getNewTokens(dto.refreshToken)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('register')
+	async register(@Body() dto: AuthDto) {
+		return this.authService.register(dto)
+	}
 }
