9d8db29fb50aa4f6fa6b926020675c6901a924a6
Rics <danil.klementev.29@gmail.com>
2025-03-05 10:12:53 +0300

    feat: add user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) {}
+
+	async byId(id: number) {
+		const user = await this.prisma.tecnUser.findUnique({
+			where: { id },
+			select: {
+
+			}
+		})
+	}
 }
