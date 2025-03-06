db3433dc0c033dabfc87fe08ebfe9fbf21d0c58d
Rics <danil.klementev.29@gmail.com>
2025-03-06 12:16:33 +0300

    feat: add select settings

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
+import { tr } from '@faker-js/faker'
 
 @Injectable()
 export class UserService {
@@ -9,7 +10,12 @@ export class UserService {
 		const user = await this.prisma.tecnUser.findUnique({
 			where: { id },
 			select: {
-
+				id: true,
+				email: true,
+				name: true,
+				avatarPath: true,
+				password: false,
+				phone: true
 			}
 		})
 	}
